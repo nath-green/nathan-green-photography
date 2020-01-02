e51c837e861b56cd366e2ad4068ab0b543d63b3f
nath-green <hello@nathgreen.co.uk>
2020-01-02 14:47:44 +0000

    add tailwind and config

--- header.php
+++ header.php
@@ -6,9 +6,7 @@
 
 		<meta name="description" content="<?php echo site_description(); ?>">
 
-		<link rel="stylesheet" href="<?php echo theme_url('/css/reset.css'); ?>">
-		<link rel="stylesheet" href="<?php echo theme_url('/css/style.css'); ?>">
-		<link rel="stylesheet" href="<?php echo theme_url('/css/small.css'); ?>" media="(max-width: 400px)">
+		<link rel="stylesheet" href="<?php echo theme_url('/css/tailwind.css'); ?>">
 
 		<link rel="alternate" type="application/rss+xml" title="RSS" href="<?php echo rss_url(); ?>">
 		<link rel="shortcut icon" href="<?php echo theme_url('img/favicon.png'); ?>">
