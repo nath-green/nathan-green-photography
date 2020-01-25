75af870583ecd287ed29c5b28e47ed8f6b0c8d05
nath-green <hello@nathgreen.co.uk>
2020-01-25 21:09:53 +0000

    fix card loop on sub pages

--- page-home.php
+++ page-home.php
@@ -163,46 +163,45 @@
 
       <?php
         $i = 1;
-        while(rwar_latest_posts(4, false)):
-        
-        ?>
-          <div class="md:w-1/2 md:px-3 mb-6" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-            <meta itemprop="url" content="<?php echo article_url(); ?>" />
-            <meta itemprop="position" content="<?php echo $i; ?>" />
-            <article class="c-card" itemtype="http://schema.org/Article">
-              <img class="h-56 w-full object-cover" src="<?php echo article_custom_field('hero'); ?>"
-                alt="<?php echo article_custom_field('image_alt'); ?>" itemprop="image" />
-              <div class="px-6 py-6 flex flex-col flex-1">
-                <header>
-                  <h2 class="mb-3" itemprop="name headline"><?php echo article_title(); ?></h2>
-                </header>
-                <div class="flex items-center mb-3">
-                  <span class="w-10 h-10 rounded-full mr-2">
-                    <img src="<?php echo user_custom_field('avatar', '', '1'); ?>"
-                      alt="Nathan Green" class="w-full h-full object-cover rounded-full">
-                  </span>
+        while(rwar_latest_posts(6, false)):
+      ?>
+        <div class="md:w-1/2 md:px-3 mb-6" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+          <meta itemprop="url" content="<?php echo article_url(); ?>" />
+          <meta itemprop="position" content="<?php echo $i; ?>" />
+          <article class="c-card" itemtype="http://schema.org/Article">
+            <img class="h-56 w-full object-cover" src="<?php echo article_custom_field('hero'); ?>"
+              alt="<?php echo article_custom_field('image_alt'); ?>" itemprop="image" />
+            <div class="px-6 py-6 flex flex-col flex-1">
+              <header>
+                <h2 class="mb-3" itemprop="name headline"><?php echo article_title(); ?></h2>
+              </header>
+              <div class="flex items-center mb-3">
+                <span class="w-10 h-10 rounded-full mr-2">
+                  <img src="<?php echo user_custom_field('avatar', '', '1'); ?>"
+                    alt="Nathan Green" class="w-full h-full object-cover rounded-full">
+                </span>
 
-                  <div>
-                    <p class="font-sans text-sm">
-                      <span itemprop="author publisher" itemscope itemtype="http://schema.org/Person">
-                        <span itemprop="name">Nathan Green</span></span> in <a
-                        href="<?php echo article_category_url(); ?>"
-                        class="text-purple-600 underline" title="Read more blog articles in this category"><?php echo article_category(); ?></a>
-                    </p>
+                <div>
+                  <p class="font-sans text-sm">
+                    <span itemprop="author publisher" itemscope itemtype="http://schema.org/Person">
+                      <span itemprop="name">Nathan Green</span></span> in <a
+                      href="<?php echo article_category_url(); ?>"
+                      class="text-purple-600 underline" title="Read more blog articles in this category"><?php echo article_category(); ?></a>
+                  </p>
 
-                    <p class="font-sans text-xs">
-                      Posted <time datetime="<?php echo date(DATE_W3C, article_time()); ?>"
-                        itemprop="datePublished dateModified"
-                        content="<?php echo date(DATE_W3C, article_time()); ?>"><?php echo relative_time(article_time()); ?></time>
-                    </p>
-                  </div>
+                  <p class="font-sans text-xs">
+                    Posted <time datetime="<?php echo date(DATE_W3C, article_time()); ?>"
+                      itemprop="datePublished dateModified"
+                      content="<?php echo date(DATE_W3C, article_time()); ?>"><?php echo relative_time(article_time()); ?></time>
+                  </p>
                 </div>
-                <p class="mb-4 flex-1" itemprop="description"><?php echo article_description(); ?></p>
-                <a class="c-btn c-btn--primary block" href="<?php echo article_url(); ?>"
-                  title="Read the full article on the blog" itemprop="url mainEntityOfPage">Read article</a>
               </div>
-            </article>
-          </div>
+              <p class="mb-4 flex-1" itemprop="description"><?php echo article_description(); ?></p>
+              <a class="c-btn c-btn--primary block" href="<?php echo article_url(); ?>"
+                title="Read the full article on the blog" itemprop="url mainEntityOfPage">Read article</a>
+            </div>
+          </article>
+        </div>
       <?php $i++; endwhile; ?>
 
     </div>
