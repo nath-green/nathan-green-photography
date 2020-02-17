2750b1bbc98b76827378cc3c2c75c8e912c1ae6d
nath-green <hello@nathgreen.co.uk>
2020-02-17 09:32:46 +0000

    add review me page

---
+++ page-review-me.php
@@ -0,0 +1,17 @@
+<?php theme_include('header'); ?>
+
+<div class="lg-container px-0 mb-10 lg:mt-10">
+  <img class="object-cover w-full" src="<?php echo theme_url('img/nathan-samuel-lg.jpg'); ?>" alt="Nathan Green holding son Samuel" />
+</div>
+
+<div class="sm-container mb-12">
+  <h1>Leave a review</h1>
+  <p class="mb-3">If you&apos;re on this page, it probably means you have had a photoshoot with me, if so thank you! I hope you loved the photos!</p>
+  <p class="mb-3">Kind words, speaking to friends and online reviews are what helps me the most.</p>
+  <p class="mb-8">If you have a spare minute, it would be amazing if you could leave a review for me on Facebook or Google. If you are feeling really generous, you could do both!</p>
+
+  <a class="c-btn c-btn--primary inline-block mr-1" href="https://www.facebook.com/pg/nathangreenphotography/reviews/" title="Leave a Facebook review on Facebook.com">Facebook review</a>
+  <a class="c-btn inline-block border-purple-600 text-purple-600" href="https://www.google.co.uk/search?q=Nathan+Green+Photography&ludocid=3971018175552524301&lsig=AB86z5XKvWO5I7Ac2AcXjX963xLV#lrd=0x48795fba1bce3ea9:0x371be3fdc66d900d,1,,," title="Leave a Google business review">Google review</a>
+</div>
+
+<?php theme_include('footer'); ?>
